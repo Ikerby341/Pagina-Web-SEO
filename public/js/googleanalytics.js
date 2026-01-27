5960488af89f44af8a2f4a187f90709c831c13e5
AngheloPJ <jhusseppp19@gmail.com>
2026-01-27 18:31:40 +0100

    change: Canvi de Google Analytics

---
+++ public/js/googleanalytics.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){ dataLayer.push(arguments); }
+
+if (typeof gtag === 'function') {
+  gtag('js', new Date());
+  gtag('config', 'G-VL404K31TZ');
+}
