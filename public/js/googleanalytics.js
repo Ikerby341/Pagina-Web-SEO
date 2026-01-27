baf0e80a414d0467fb9f627f22d73d947a60c2b3
AngheloPJ <jhusseppp19@gmail.com>
2026-01-27 18:34:25 +0100

    🦍

--- public/js/googleanalytics.js
+++
@@ -1,7 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){ dataLayer.push(arguments); }
-
-if (typeof gtag === 'function') {
-  gtag('js', new Date());
-  gtag('config', 'G-VL404K31TZ');
-}
